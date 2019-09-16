5d969ba05d94c09c2bf7e17cf84fa41b94247343
Harshit Kumar Sankhla <harshitsankhla@hotmail.com>
2019-09-17 00:16:11 +0530

    Update drone.sh

--- drone.sh
+++ drone.sh
@@ -81,7 +81,7 @@ source $HOME/.bashrc
 
 # MAVROS
 cd $HOME
-sudo apt-get install ros-kinetic-mavros ros-kinetic-mavros-extras
+sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 sudo chmod +x install_geographiclib_datasets.sh
 ./install_geographiclib_datasets.sh
